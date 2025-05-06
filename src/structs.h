d7261eba2c6a2821bd2487f0e7e0494b8d77b24c
Matanel <matanel.discord@gmail.com>
2025-05-06 18:34:49 +0300

    Add files via upload

--- src/structs.h
+++ src/structs.h
@@ -4,23 +4,6 @@
 //#include <winioctl.h>  // Add this at the top with other includes
 // Ensure proper alignment for structures
 #pragma pack(push, 8)
-
-typedef struct _LDR_DATA_TABLE_ENTRY {
-    LIST_ENTRY InLoadOrderLinks;
-    LIST_ENTRY InMemoryOrderLinks;
-    LIST_ENTRY InInitializationOrderLinks;
-    PVOID DllBase;
-    PVOID EntryPoint;
-    ULONG SizeOfImage;
-    UNICODE_STRING FullDllName;
-    UNICODE_STRING BaseDllName;
-    ULONG Flags;
-    USHORT LoadCount;
-    USHORT TlsIndex;
-    LIST_ENTRY HashLinks;
-    ULONG TimeDateStamp;
-} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
-
 #pragma pack(pop)
 
 // Add memory barrier macros for synchronization
