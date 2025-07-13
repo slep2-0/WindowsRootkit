6ebda368063e068895b9c437239957694863f9f5
Matanel <195588478+slep2-0@users.noreply.github.com>
2025-07-14 01:47:03 +0300

    Add files via upload

--- src/HookingUtils.cpp
+++ src/HookingUtils.cpp
@@ -4,6 +4,9 @@ static BOOLEAN(*OrgMmIsAddressValid)(PVOID ADDRESS) = nullptr;
 static NTSTATUS(*OrgMmCopyVirtualMemory)(PEPROCESS SourceProcess, PVOID SourceAddress, PEPROCESS TargetProcess, PVOID TargetAddress, SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode, PSIZE_T ReturnSize) = nullptr;
 static NTSTATUS(*OrgPsLookupProcessByProcessId)(HANDLE PID, PEPROCESS *Process) = nullptr;
 static NTSTATUS(*OrgZwQueryVirtualMemory)(HANDLE ProcessHandle, PVOID BaseAddressOpt, MEMORY_INFORMATION_CLASS MemoryInformationClass, PVOID MemoryInformationOut, SIZE_T MemoryInformationLength, PSIZE_T ReturnLengthOutOpt);
+static NTSTATUS(*OrgNtQueryVirtualMemory)(HANDLE ProcessHandle, PVOID BaseAddress, MEMORY_INFORMATION_CLASS MemoryInformationClass, PVOID MemoryInformation, SIZE_T MemoryInformationLength, PSIZE_T ReturnLength);
+static NTSTATUS(*OrgZwProtectVirtualMemory)(HANDLE ProcessHandle, PVOID* BaseAddress, SIZE_T* NumberOfBytesToProtect, ULONG NewAccessProtection, PULONG OldAccessProtection);
+static NTSTATUS(*OrgNtAllocateVirtualMemory)(HANDLE ProcessHandle, PVOID* BaseAddress, ULONG_PTR ZeroBits, PSIZE_T RegionSize, ULONG AllocationType, ULONG Protect);
 static volatile ADDRESS_RANGE g_BlockedAddress = { 0x0, 0x0 };
 static volatile UINT32 g_PidToAddressBlock = 0;
 static volatile UINT32 g_PidToBlock = 0;
@@ -12,6 +15,10 @@ static bool hasHookedMmIsAddressValid = false;
 static bool hasHookedPsLookupByProcessId = false;
 static bool hasHookedMmCopyVirtualMemory = false;
 static bool hasHookedZwQueryVirtualMemory = false;
+static bool hasHookedNtQueryVirtualMemory = false;
+static bool hasHookedZwProtectVirtualMemory = false;
+static bool hasHookedNtAllocateVirtualMemory = false;
+static bool hasGloballyHooked = false;
 
 WCHAR* ExtractFileNameFully(const WCHAR* fullPath) {
     if (!fullPath) return NULL;
@@ -68,9 +75,45 @@ NTSTATUS HookedZwQueryVirtualMemory(HANDLE ProcessHandle, PVOID BaseAddressOpt, 
             return STATUS_INVALID_PARAMETER; // ZwQueryVirtualMemory returns 5 values, first is STATUS_SUCCESS, and the other most safe one is STATUS_INVALID_PARAMETER, let's return that.
         }
     }
-
-    DbgPrint("[HOOK-ERROR] ObReferenceObjectByHandle failed, STATUS: %d\n", status);
+    DbgPrint("[HOOK-ERROR] HookedZwQueryVirtualMemory: ObReferenceObjectByHandle failed, STATUS: %d\n", status);
     return OrgZwQueryVirtualMemory(ProcessHandle, BaseAddressOpt, MemoryInformationClass, MemoryInformationOut, MemoryInformationLength, ReturnLengthOutOpt);
+}
+
+// technically only this has to be hooked and not also ZwQueryVirtualMemory as the Zw version just handles the transition, at the end it also arrives at the Nt version, but who cares.
+NTSTATUS HookedNtQueryVirtualMemory(HANDLE ProcessHandle, PVOID BaseAddressOpt, MEMORY_INFORMATION_CLASS MemoryInformationClass, PVOID MemoryInformationOut, SIZE_T MemoryInformationLength, PSIZE_T ReturnLengthOutOpt) {
+    // since we only have a HANDLE we reference it to a PID using Ob.
+    PEPROCESS process;
+    NTSTATUS status = ObReferenceObjectByHandle(ProcessHandle, PROCESS_ALL_ACCESS, *PsProcessType, KernelMode, (PVOID*)&process, NULL);
+
+    if (status == STATUS_SUCCESS) {
+        HANDLE PIDhandle = PsGetProcessId(process);
+        UINT32 PID = HandleToUlong(PIDhandle);
+        if (PID == g_PidToBlock) {
+            DbgPrint("[HOOK] NtQueryVirtualMemory called with protected PID: %d | BLOCKED.\n", PID);
+            return STATUS_INVALID_PARAMETER; // ZwQueryVirtualMemory returns 5 values, first is STATUS_SUCCESS, and the other most safe one is STATUS_INVALID_PARAMETER, let's return that.
+        }
+    }
+    DbgPrint("[HOOK-ERROR] HookedNtQueryVirtualMemory:  ObReferenceObjectByHandle failed, STATUS: %d\n", status);
+    return OrgZwQueryVirtualMemory(ProcessHandle, BaseAddressOpt, MemoryInformationClass, MemoryInformationOut, MemoryInformationLength, ReturnLengthOutOpt);
+}
+
+NTSTATUS HookedZwProtectVirtualMemory(HANDLE ProcessHandle, PVOID* BaseAddress, SIZE_T* NumberOfBytesToProtect, ULONG NewAccessProtection, PULONG OldAccessProtection) {
+    //remember this is a pointer to a pointer, so we dereference to get the ACTUAL base address.
+    UINT64 givenAddress = reinterpret_cast<UINT64>(*BaseAddress);
+    if (IsAddressInRange(givenAddress)) {
+        DbgPrint("[HOOK] ZwProtectVirtualMemory called with address: 0x%llx | BLOCKED RANGE: 0x%llx - 0x%llx\n", givenAddress, g_BlockedAddress.Start, g_BlockedAddress.End);
+        return STATUS_INVALID_PARAMETER;
+    }
+    return OrgZwProtectVirtualMemory(ProcessHandle, BaseAddress, NumberOfBytesToProtect, NewAccessProtection, OldAccessProtection);
+}
+
+NTSTATUS HookedNtAllocateVirtualMemory(HANDLE ProcessHandle, PVOID* BaseAddress, ULONG_PTR ZeroBits, PSIZE_T RegionSize, ULONG AllocationType, ULONG Protect) {
+    UINT64 givenAddress = reinterpret_cast<UINT64>(*BaseAddress);
+    if (IsAddressInRange(givenAddress)) {
+        DbgPrint("[HOOK] NtAllocateVirtualMemory called with address: 0x%llx | BLOCKED RANGE: 0x%llx - 0x%llx\n", givenAddress, g_BlockedAddress.Start, g_BlockedAddress.End);
+        return STATUS_INVALID_PARAMETER;
+    }
+    return OrgNtAllocateVirtualMemory(ProcessHandle, BaseAddress, ZeroBits, RegionSize, AllocationType, Protect);
 }
 
 NTSTATUS HookedPsLookupProcessByProcessId(HANDLE PID, PEPROCESS* Process) {
@@ -93,7 +136,6 @@ NTSTATUS HookedPsLookupProcessByProcessId(HANDLE PID, PEPROCESS* Process) {
     // return orig function if the pid is the blocked one.
     return OrgPsLookupProcessByProcessId(PID, Process);
 }
-
 NTSTATUS HookingUtils::HookMemory(UINT32 PID) {
     DbgPrint("[+] HookMemory called.\n");
     ADDRESS_RANGE addressToBlock;
@@ -132,9 +174,10 @@ NTSTATUS HookingUtils::HookMemory(UINT32 PID) {
 
     addressToBlock.Start = baseAddr;
     addressToBlock.End = endAddr;
-
-    if (hasHookedMmIsAddressValid && hasHookedMmCopyVirtualMemory) {
+    
+    if (hasGloballyHooked) {
         //InterlockedExchange64((volatile LONG64*)&g_BlockedAddress, addressToBlock);
+        g_PidToAddressBlock = PID;
         SetRange(addressToBlock.Start, addressToBlock.End);
         return STATUS_SUCCESS;
     }
@@ -157,20 +200,43 @@ NTSTATUS HookingUtils::HookMemory(UINT32 PID) {
         DbgPrint("[-] STATUS_INVALID_ADDRESS returning, OrgZwQueryVirtualMemory is not valid\n");
         return STATUS_INVALID_ADDRESS;
     }
+    UNICODE_STRING routineNtQueryVirtualMem = RTL_CONSTANT_STRING(L"NtQueryVirtualMemory");
+    OrgNtQueryVirtualMemory = (decltype(OrgNtQueryVirtualMemory))MmGetSystemRoutineAddress(&routineNtQueryVirtualMem);
+    if (!OrgNtQueryVirtualMemory) {
+        DbgPrint("[-] STATUS_INVALID_ADDRESS returning, OrgNtQueryVirtualMemory is not valid\n");
+        return STATUS_INVALID_ADDRESS;
+    }
+    UNICODE_STRING routineZwProtectVirtualMem = RTL_CONSTANT_STRING(L"ZwProtectVirtualMemory");
+    OrgZwProtectVirtualMemory = (decltype(OrgZwProtectVirtualMemory))MmGetSystemRoutineAddress(&routineZwProtectVirtualMem);
+    if (!OrgZwProtectVirtualMemory) {
+        DbgPrint("[-] STATUS_INVALID_ADDRESS returning, OrgZwProtectVirtualMemory is not valid\n");
+        return STATUS_INVALID_ADDRESS;
+    }
+    UNICODE_STRING routineNtAllocateVirtualMem = RTL_CONSTANT_STRING(L"NtAllocateVirtualMemory");
+    OrgNtAllocateVirtualMemory = (decltype(OrgNtAllocateVirtualMemory))MmGetSystemRoutineAddress(&routineNtAllocateVirtualMem);
+    if (!OrgNtAllocateVirtualMemory) {
+        DbgPrint("[-] STATUS_INVALID_ADDRESS returning, OrgNtAllocateVirtualMemory is not valid\n");
+        return STATUS_INVALID_ADDRESS;
+    }
     DetourTransactionBegin();
     DetourUpdateThread(ZwCurrentThread());
     DetourAttach((void**)&OrgMmIsAddressValid, HookedMmIsAddressValid);
     DetourAttach((void**)&OrgMmCopyVirtualMemory, HookedMmCopyVirtualMemory);
     DetourAttach((void**)&OrgZwQueryVirtualMemory, HookedZwQueryVirtualMemory);
+    DetourAttach((void**)&OrgNtQueryVirtualMemory, HookedNtQueryVirtualMemory);
+    DetourAttach((void**)&OrgZwProtectVirtualMemory, HookedZwProtectVirtualMemory);
+    DetourAttach((void**)&OrgNtAllocateVirtualMemory, HookedNtAllocateVirtualMemory);
     DetourTransactionCommit();
-    DbgPrint("[+] Detours ended - MmIsAddressValid\n");
-    DbgPrint("[+] Detours ended - MmCopyVirtualMemory\n");
-    DbgPrint("[+] Detours ended - ZwQueryVirtualMemory\n");
+    DbgPrint("[+] Detours ended - Memory Hooking\n");
     //InterlockedExchange64((volatile LONG64*)&g_BlockedAddress, addressToBlock);
     SetRange(addressToBlock.Start, addressToBlock.End);
     g_PidToAddressBlock = PID;
     hasHookedMmIsAddressValid = true;
     hasHookedMmCopyVirtualMemory = true;
+    hasHookedNtQueryVirtualMemory = true;
+    hasHookedZwProtectVirtualMemory = true;
+    hasHookedNtAllocateVirtualMemory = true;
+    hasGloballyHooked = true;
     DbgPrint("[+] Finished with hooking memory functions for process.\n");
     return STATUS_SUCCESS;
 }
@@ -214,6 +280,16 @@ NTSTATUS HookingUtils::DeleteAllHooks() {
     if (hasHookedZwQueryVirtualMemory) {
         DetourDetach((void**)&OrgZwQueryVirtualMemory, HookedZwQueryVirtualMemory);
     }
+    if (hasHookedZwQueryVirtualMemory) {
+        DetourDetach((void**)&OrgNtQueryVirtualMemory, HookedNtQueryVirtualMemory);
+    }
+    if (hasHookedZwProtectVirtualMemory) {
+        DetourDetach((void**)&OrgZwProtectVirtualMemory, HookedZwProtectVirtualMemory);
+    }
+    if (hasHookedNtAllocateVirtualMemory) {
+        DetourDetach((void**)&OrgNtAllocateVirtualMemory, HookedNtAllocateVirtualMemory);
+    }
+    hasGloballyHooked = false;
     DetourTransactionCommit();
     DbgPrint("[+] Hooks successfully deleted.\n");
     return STATUS_SUCCESS;
--- src/HookingUtils.h
+++ src/HookingUtils.h
@@ -6,6 +6,7 @@ typedef struct _ADDRESS_RANGE {
     UINT64 End;
 } ADDRESS_RANGE;
 
+// i did not hook NtFreeVirtualMemory, to atleast let the kernel free the process memory after use (unless he uses some internal function, I'm guessing he uses NtFreePhysicalMemory or some equivalent to the name, instead of virtual, and just deletes the virtual pages in the table.) -- It's fine to let the free memory stay, as you cant use the memory after without Alloc, which is hooked.
 namespace HookingUtils {
 	NTSTATUS HookMemory(UINT32 PID);
     NTSTATUS HookPsLookupProcessByProcessId(UINT32 PidToBlock);
